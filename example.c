03186d58ee8c67d448015458028fb325289dc63a
Mike Ramsy <3losold2@protonmail.ch>
2017-08-16 19:44:10 -0700

    Create example.c

---
+++ example.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "ezBase64.h"
+
+int main()
+{
+	char*Greeting = "Hello World";
+	char*Encoded = ezBase64Encode(Greeting,strlen(Greeting));
+	char*Decoded = ezBase64Decode(Encoded,strlen(Encoded));
+	printf("%s\n%s\n%s\n",Greeting,Encoded,Decoded);
+	system("pause");
+	return 0;
+}
+
